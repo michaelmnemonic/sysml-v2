8e4a5db3874c460c8601658fa6f801eb67627ee6
michaelmnemonic <michaelmnemonic@posteo.de>
2025-07-01 22:50:02 +0200

    Partdefinitions are supported.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from sysml2py import Model
+from sysml2py import load_grammar as loads
+from sysml2py.formatting import classtree
 
 def main():
-    with open("model/structure.sysml", "r") as f:
-        model = Model().load(f)
+    text = """package Structure {
+        // PartDefinition not implemted ;-(
+        part def Drone ;
 
-    print(model.dump())
+        part drone {
+            attribute batteryLevel;
+
+            // portUsage not supported?
+            //port power;
+        }
+        part warehouse {
+            part deliveryPackages;
+        }
+    }"""
+    model = loads(text)
+    print(classtree(model).dump())
 
 if __name__ == "__main__":
     main()
