e77eec964e8b008d2423f2248998e616f0bde91d
michaelmnemonic <michaelmnemonic@posteo.de>
2025-07-01 22:04:39 +0200

    Minimal example

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from sysml-v2!")
+from sysml2py import Attribute, Part, Item, Model, Package
 
+def main():
+    with open("model/structure.sysml", "r") as f:
+        model = Model().load(f)
+
+    print(model.dump())
 
 if __name__ == "__main__":
     main()
